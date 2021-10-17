ddaca237712b228dfd174084125a0bacc7566d40
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-17 15:48:39 -0300

    Aula 10 Atribuição Simplificada

---
+++ Aula 10 Atribuicao Simplificada/Aula 10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int x=10, y=10;
+	
+	y+=x;
+//	y-=x;
+//	x*=y;
+//	x/=y;
+//  x%=y;
+	printf("%d \n", y);
+	system("pause");
+	return 0;
+}
