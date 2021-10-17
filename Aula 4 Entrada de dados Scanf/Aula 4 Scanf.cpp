01682ddc9e78221928691feda9dd6bc84602918c
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-17 13:12:03 -0300

    Aula 4 Scanf

---
+++ Aula 4 Entrada de dados Scanf/Aula 4 Scanf.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char letra;
+	int num;
+	printf("Digite uma Letra e Numero:\n");
+	scanf("%c%d",&letra,&num);
+	printf("A letra e: %c, Numero e: %d ",letra,num);
+	return 0;
+}
