fb0040836b82b6f5f508afd840876cba4c3642ad
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-17 13:27:47 -0300

    Aula 5 Atribuição

---
+++ Aula 5 Operador de Atribuicao/Aula 5 Atribuicao .c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+//	Atribuições 
+//	int x = 5, y;
+//	y= x;
+//	y= x+10;
+//	
+//	Atribuiçoes multiplas
+//	int p=b=q=a=10; //Quer dizer que o valor de A=10 e o Valor de Q = o valor de A e ai vai ...
+	
+	int x= 97;
+	char ch1,ch2='A';
+	ch1=x;
+	printf("ch1 = %c \n",ch1);
+	x=ch2;
+	printf("x = %d \n", x);
+		
+	system("pause");
+	return 0;
+}
+
+
