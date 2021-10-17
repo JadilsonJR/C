fb2a32031a43f4f5f6b2b8fac6d1d666bd7f8433
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-17 15:56:49 -0300

    Aula 11 Operadores Relacionais

---
+++ Aula 11 Operadores Relacionais/Aula 11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+//	Operadores Relacionais 
+//	> "Maior que "
+//	< "Menor que "
+//	>= "Maior ou igual a"
+//	<= "Menor ou igual a"
+//	!= "Diferente"
+//	== "Igual"
+	
+	int x=5, y=3;
+	
+	printf("Resultado = %d \n ", x>4); // 1 Para True, 0 Para False 
+	printf("Resultado = %d \n ", x >= y+2);
+	system("pause");
+	return 0;
+}
