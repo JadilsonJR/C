eae34a770d47003e5ab6f7f2e8e43c052c0d26d1
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-17 13:11:27 -0300

    Aula 2 Tipos de Variáveis

---
+++ Aula 2 Variaveis/Aula 2 tipos de variaveis e inicializacao.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+//	Tipos de Variaveis char, int, float, double,
+//  Nomeacao de variaveis Pode: A,b,9,_  Não Pode: á, !,1n, palavras reservadas
+//  Case sensentive 
+
+ char letra = "a";
+ int num = 10;
+ float  num2 = 5.0;
+ double num3 = 5.0e3;
+// NO double estamos escrevendo na conotação cientefica é como se fosse 5 elevado a 10 ao cubo
+ 
+ int n1,n2,n3,n4, n5=10; // Podemos inicializar diviersas variaveis de uma vez só 
+
+}
