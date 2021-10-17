0e00729fec0fb797158c54a3ed55a635e03a7708
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-17 17:26:39 -0300

    Aula 13 Comando IF

---
+++ Aula 13 Comando IF/Aula 13 Comando IF.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int x;
+	printf("Digite um Valor inteiro:");
+	scanf("%d",&x);
+	if(x>0){
+		printf("O valor e positivo !\n");
+	}
+	
+	printf("Fim do Programa\n");
+	
+	system("pause");
+	return 0;
+}
