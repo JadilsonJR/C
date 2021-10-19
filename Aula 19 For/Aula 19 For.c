54b9d3dc319b436612bba0960d27f67419bee92e
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-18 23:02:09 -0300

    Aula 19 For

---
+++ Aula 19 For/Aula 19 For.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a,b,c;
+	printf("Digite dois valores inteiros: ");
+	scanf("%d %d",&a,&b);
+	for(c = a; c <=b ; c++){
+		printf("%d\n",c);
+	}
+	printf("Fim do Programa \n");
+	system("pause");
+	return 0;
+}
