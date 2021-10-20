5813db26591e632f7bf03536c718fa0a63db30c8
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-19 22:39:36 -0300

    Aula 22 Break

---
+++ Aula 22 Break/Aula 22 Break.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a,b;
+	printf("Digite o valor de A:");
+	scanf("%d",&a);
+	printf("Digite o valor de B:");
+	scanf("%d",&b);
+	while(a<=b){
+		a+=1;
+		if(a==5){
+			break;
+		}
+		printf("%d\n",a);
+	
+	}
+		
+	
+	system("pause");
+	return 0;
+}
