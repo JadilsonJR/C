fb142357779d0ecb76b5c026dedbeffb5d7e8fa3
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2021-10-19 21:46:06 -0300

    Adição de uma função

--- Aula 20 Do While/Aula 20 Do While.c
+++ Aula 20 Do While/Aula 20 Do While.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int menu();
+
 int main(){
 	int a,b;
 	printf("Digite dois valores inteiros: ");
@@ -9,9 +11,23 @@ int main(){
 		printf("%d \n",a);
 		a++;
 	}while(a<b);
-	
+	menu();
 	printf("Fim do Programa!\n");
-		
+	
+	system("pause");
+	return 0;
+}
+
+int menu(){
+	int i;
+	do{
+		printf("Escolha uma Opcao: \n");
+		printf("( 1 ) Opcao 1 \n");
+		printf("( 2 ) Opcao 2 \n");
+		printf("( 3 ) Opcao 3 \n");	
+		scanf("%d",&i);
+	}while((i<1) || (i>3));
+	printf("Voce escolheu a opcao: %d \n",i);
 	system("pause");
 	return 0;
 }
